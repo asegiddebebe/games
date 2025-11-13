56079c0de5a3b690aa00063287e036e33c2db97d
Asegid Debebe Feyissa <asegid@gmail.com>
2025-11-13 13:58:31 +0300

    Change database configuration to PostgreSQL
    Updated database settings to use PostgreSQL instead of SQLite.

--- insight_api/settings.py
+++ insight_api/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'insight_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',    # or 'django.db.backends.postgresql_psycopg2' in older Django versions
+        'NAME': 'games_db',                 # the name of your PostgreSQL database
+        'USER': 'games_api',                 # the PostgreSQL user you've created / granted access
+        'PASSWORD': 'games@123',                  # that user’s password
+        'HOST': 'localhost',                          # or an IP/hostname if remote
+        'PORT': '5432',                               # PostgreSQL’s default port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
